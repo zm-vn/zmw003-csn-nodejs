b355718af8917cde9ffdf73c8db857c1c1d80fed
LamNX <lamnx@miichisoft.net>
2023-11-02 11:17:20 +0700

    add port config

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,6 +1,12 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'test' | 'development' | 'staging' | 'production'
-    readonly PORT: number
+    readonly PORT: string
+
+    readonly DB_HOST: string
+    readonly DB_USER: string
+    readonly DB_PASSWORD: string
+    readonly DB_NAME: string
+    readonly DB_PORT: string
   }
 }
