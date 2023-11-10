d155d64e92cf4c94fe3774236e77c49187d84a74
LamNX <lamnx@miichisoft.net>
2023-11-10 15:32:24 +0700

    add cookie support

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,6 +21,7 @@ export class AuthService {
     return {
       accessToken,
       refreshToken,
+      user,
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+  app.use(cookieParser())
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
